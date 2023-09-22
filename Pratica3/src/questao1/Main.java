7514a8bb94bbf7a3dfa12e315a7cd3e0af1f27d6
Arthur Henrique Fernandes <114191292+Arthurhff@users.noreply.github.com>
2023-09-21 22:14:49 -0300

    Pratica 3 - POO

---
+++ Pratica3/src/questao1/Main.java
@@ -0,0 +1,17 @@
+package questao1;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Normal n1 = new Normal(100);
+        n1.imprimeIngresso();
+
+        Vip v1 = new Vip(100, 50);
+        v1.imprimeIngresso();
+
+        Camarote c1 = new Camarote(100, 100, "Setor Camarote" );
+        c1.imprimeIngresso();
+
+    }
+}
